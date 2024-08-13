d07c4dca6757fff742ffa603314a66f4a20f9f2a
Joan Wee <jwee@indeed.com>
2024-08-14 05:38:14 +0900

    Updates

---
+++ precache-manifest.92512878b6d1fce7f02d172b753d4109.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba8a72c706b0548a048293ee1953ec1a",
+    "url": "/flashcard-assessment/index.html"
+  },
+  {
+    "revision": "6d56c621963928b82bbf",
+    "url": "/flashcard-assessment/static/css/main.c6001462.chunk.css"
+  },
+  {
+    "revision": "366761a1b4349a618642",
+    "url": "/flashcard-assessment/static/js/2.6be03760.chunk.js"
+  },
+  {
+    "revision": "ea6bead85db5d7afa70f852e6d6e5ea1",
+    "url": "/flashcard-assessment/static/js/2.6be03760.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d56c621963928b82bbf",
+    "url": "/flashcard-assessment/static/js/main.85dbb1b0.chunk.js"
+  },
+  {
+    "revision": "4f2af1b8db50767e2f57",
+    "url": "/flashcard-assessment/static/js/runtime-main.5a43cdff.js"
+  }
+]);
